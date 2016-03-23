9fd1e4052e6b254bcd344ec1128faac02519dd4d
Laurent Defert <lds@stamus-networks.com>
2016-03-23 16:54:52 +0100

    Prefix dashboards names with "SN"
    Closes #15

--- load.sh
+++ load.sh
@@ -4,6 +4,11 @@ set -x
 
 cd $(dirname $0)
 
+function get_name()
+{
+	basename "$1" .json | sed -e 's/ /%20/g'
+}
+
 if [ -z "$1" ]; then
     ELASTICSEARCH=http://127.0.0.1:9200
 else
@@ -24,39 +29,39 @@ $CURL -XDELETE $ELASTICSEARCH/.kibana/ ||:
 
 for file in $DIR/index-pattern/*.json
 do
-    name=`basename "$file" .json`
+    name="$(get_name "$file")"
     echo "Loading index pattern $name:"
 
     $CURL -XPOST "$ELASTICSEARCH/.kibana/index-pattern/$name" \
-        -d @$file || exit 1
+        -d "@$file" || exit 1
     echo
 done
 
 
 for file in $DIR/search/*.json
 do
-    name=`basename $file .json`
+    name="$(get_name "$file")"
     echo "Loading search $name:"
-    $CURL -XPUT $ELASTICSEARCH/.kibana/search/$name \
-        -d @$file || exit 1
+    $CURL -XPUT "$ELASTICSEARCH/.kibana/search/$name" \
+        -d "@$file" || exit 1
     echo
 done
 
 for file in $DIR/visualization/*.json
 do
-    name=`basename $file .json`
+    name="$(get_name "$file")"
     echo "Loading visualization $name:"
-    $CURL -XPUT $ELASTICSEARCH/.kibana/visualization/$name \
-        -d @$file || exit 1
+    $CURL -XPUT "$ELASTICSEARCH/.kibana/visualization/$name" \
+        -d "@$file" || exit 1
     echo
 done
 
 for file in $DIR/dashboard/*.json
 do
-    name=`basename $file .json`
+    name="$(get_name "$file")"
     echo "Loading dashboard $name:"
-    $CURL -XPUT $ELASTICSEARCH/.kibana/dashboard/$name \
-        -d @$file || exit 1
+    $CURL -XPUT "$ELASTICSEARCH/.kibana/dashboard/$name" \
+        -d "@$file" || exit 1
     echo
 done
 
